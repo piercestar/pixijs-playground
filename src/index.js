5ca23a476110a3be578e1a9495991606a9d37648
piercestar <kong_terence@hotmail.com>
2021-04-30 07:02:18 +0800

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// Import Application class that is the main part of our PIXI project
+// import { Application } from '@pixi/app'
+// import { Renderer } from '@pixi/core' // Renderer is the class that is going to register plugins
+// import { BatchRenderer } from '@pixi/core' // BatchRenderer is the "plugin" for drawing sprites
+// import { TickerPlugin } from '@pixi/ticker' // TickerPlugin is the plugin for running an update loop (it's for the application class)
+// import { AppLoaderPlugin } from '@pixi/loaders'
+// import { Sprite } from '@pixi/sprite'
+// import { Container } from '@pixi/display'
+// import { Graphics } from '@pixi/graphics'
+import { App } from './core/app';
+
+const app = new App();
---
+++ webpack.config.dev.js
@@ -0,0 +1,29 @@
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  devServer: {
+    contentBase: "dist",
+    port: 3000,
+  },
+  devtool: "inline-source-map",
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+            from: "src/assets",
+            to: "assets",
+        },
+        {
+            from: "src/core",
+            to: "core",
+        },
+      ],
+    }),
+    new HTMLWebpackPlugin({
+      template: "src/index.html",
+      filename: "index.html",
+    }),
+  ],
+};
